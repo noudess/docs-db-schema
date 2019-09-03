596acd9e05c9294b902b846c33f93b086ef0502a
Akkadius <akkadius1@gmail.com>
2019-09-03 03:23:28 -0500

    Update doc-gen.js

--- scripts/doc-gen.js
+++ scripts/doc-gen.js
@@ -1,3 +1,9 @@
+/**
+ * Quick and dirty doc generator script
+ *
+ * @author: Akkadius
+ */
+
 const mysql    = require('mysql');
 const yaml     = require('js-yaml');
 const fs       = require('fs');
@@ -18,6 +24,9 @@ connection.connect();
 let schemaData                  = {};
 const SCHEMA_REFERENCE_YML_FILE = 'database-schema-reference.yml';
 
+/**
+ * Tables to be excluded from the 'pull' command
+ */
 const excludedTables = [
   'tblServerListType',
   'tblServerAdminRegistration',
@@ -26,11 +35,39 @@ const excludedTables = [
   'zone_state_dump'
 ];
 
+
+/**
+ * To update which categories a table falls under, update this
+ */
+const tableCategories = {
+  'Tasks': [
+    'tasks',
+    'tasksets',
+    'task_activities',
+    'task_replay_groups'
+  ],
+  'Guilds': [
+    'guilds',
+    'guild_bank',
+    'guild_ranks',
+    'guild_members',
+    'guild_relations'
+  ]
+};
+
+let tablesWrittenToIndex = {};
+
+/**
+ * Load an existing database yml reference
+ */
 if (fs.existsSync(SCHEMA_REFERENCE_YML_FILE)) {
   schemaData = yaml.safeLoad(fs.readFileSync(SCHEMA_REFERENCE_YML_FILE, 'utf8'));
   console.log('Loaded schema from [' + SCHEMA_REFERENCE_YML_FILE + ']');
 }
 
+/**
+ * Pull
+ */
 program
   .version('1.0.0')
   .command('pull')
@@ -49,51 +86,52 @@ program
           ORDINAL_POSITION;
       `, function (error, results, fields) {
           results.forEach(function (row) {
-            /**
-             RowDataPacket {
-                TABLE_CATALOG: 'def',
-                TABLE_SCHEMA: 'peq',
-                TABLE_NAME: 'aa_ability',
-                COLUMN_NAME: 'category',
-                ORDINAL_POSITION: 3,
-                COLUMN_DEFAULT: '-1',
-                IS_NULLABLE: 'NO',
-                DATA_TYPE: 'int',
-                CHARACTER_MAXIMUM_LENGTH: null,
-                CHARACTER_OCTET_LENGTH: null,
-                NUMERIC_PRECISION: 10,
-                NUMERIC_SCALE: 0,
-                DATETIME_PRECISION: null,
-                CHARACTER_SET_NAME: null,
-                COLLATION_NAME: null,
-                COLUMN_TYPE: 'int(10)',
-                COLUMN_KEY: '',
-                EXTRA: '',
-                PRIVILEGES: 'select,insert,update,references',
-                COLUMN_COMMENT: '',
-                IS_GENERATED: 'NEVER',
-                GENERATION_EXPRESSION: null
+              /**
+               RowDataPacket {
+                  TABLE_CATALOG: 'def',
+                  TABLE_SCHEMA: 'peq',
+                  TABLE_NAME: 'aa_ability',
+                  COLUMN_NAME: 'category',
+                  ORDINAL_POSITION: 3,
+                  COLUMN_DEFAULT: '-1',
+                  IS_NULLABLE: 'NO',
+                  DATA_TYPE: 'int',
+                  CHARACTER_MAXIMUM_LENGTH: null,
+                  CHARACTER_OCTET_LENGTH: null,
+                  NUMERIC_PRECISION: 10,
+                  NUMERIC_SCALE: 0,
+                  DATETIME_PRECISION: null,
+                  CHARACTER_SET_NAME: null,
+                  COLLATION_NAME: null,
+                  COLUMN_TYPE: 'int(10)',
+                  COLUMN_KEY: '',
+                  EXTRA: '',
+                  PRIVILEGES: 'select,insert,update,references',
+                  COLUMN_COMMENT: '',
+                  IS_GENERATED: 'NEVER',
+                  GENERATION_EXPRESSION: null
+                }
+               */
+
+              if (excludedTables.includes(row.TABLE_NAME)) {
+                return;
               }
-             */
 
-            if (excludedTables.includes(row.TABLE_NAME)) {
-              return;
-            }
+              if (typeof schemaData[row.TABLE_NAME] === 'undefined') {
+                schemaData[row.TABLE_NAME] = {};
+              }
+              if (typeof schemaData[row.TABLE_NAME][row.COLUMN_NAME] === 'undefined') {
+                schemaData[row.TABLE_NAME][row.COLUMN_NAME] = {};
+              }
 
-            if (typeof schemaData[row.TABLE_NAME] === 'undefined') {
-              schemaData[row.TABLE_NAME] = {};
-            }
-            if (typeof schemaData[row.TABLE_NAME][row.COLUMN_NAME] === 'undefined') {
-              schemaData[row.TABLE_NAME][row.COLUMN_NAME] = {};
-            }
+              schemaData[row.TABLE_NAME][row.COLUMN_NAME].dataType = row.DATA_TYPE;
+              schemaData[row.TABLE_NAME][row.COLUMN_NAME].nullable = row.IS_NULLABLE;
 
-            schemaData[row.TABLE_NAME][row.COLUMN_NAME].dataType = row.DATA_TYPE;
-            schemaData[row.TABLE_NAME][row.COLUMN_NAME].nullable = row.IS_NULLABLE;
-
-            if (typeof schemaData[row.TABLE_NAME][row.COLUMN_NAME].description === 'undefined') {
-              schemaData[row.TABLE_NAME][row.COLUMN_NAME].description = '';
+              if (typeof schemaData[row.TABLE_NAME][row.COLUMN_NAME].description === 'undefined') {
+                schemaData[row.TABLE_NAME][row.COLUMN_NAME].description = '';
+              }
             }
-          });
+          );
 
           /**
            * Write .yml
@@ -106,15 +144,29 @@ program
     }
   );
 
+/**
+ * Write
+ */
 program.command('write')
   .description('Writes the schema reference into markdown files')
   .action(function (cmd) {
 
-      let summaryFileContents = `# Summary
+      let summaryTablesList  = '## Tables' + '\n\n';
+      let categoryTablesList = '';
 
-## Tables
+      Object.keys(tableCategories).forEach(function (category) {
+        var tables = tableCategories[category];
 
-`;
+        console.log(category);
+        categoryTablesList += '\n* [' + category + '](placeholder.md)\n';
+        // categoryTablesList += '\n## ' + category + '\n\n';
+
+        tables.forEach(function (table) {
+          tablesWrittenToIndex[table] = 1;
+          // categoryTablesList += '* [' + table + '](' + table + '.md)' + '\n';
+          categoryTablesList += '    * [' + table + '](' + table + '.md)' + '\n';
+        });
+      });
 
       Object.keys(schemaData).forEach(function (key) {
           var val         = schemaData[key];
@@ -148,9 +200,26 @@ program.command('write')
             }
           );
 
-          summaryFileContents += "* [" + tableName + "](" + tableName + ".md)\n";
+          if (tablesWrittenToIndex[tableName]) {
+            return;
+          }
+
+          summaryTablesList += '* [' + tableName + '](' + tableName + '.md)\n';
         }
       );
+
+      /**
+       * Base summary file contents
+       *
+       * @type {string}
+       */
+      let summaryFileContents = `# Summary
+
+${categoryTablesList}
+${summaryTablesList}
+
+
+`;
 
       /**
        * Write summary
@@ -167,12 +236,26 @@ program.command('write')
 
 program.parse(process.argv);
 
+/**
+ * Some date function shamelessly ripped from stackoverflow (quick and dirty)
+ *
+ * @param date
+ * @returns {string}
+ */
 function formatDate(date) {
   var monthNames = [
-    'January', 'February', 'March',
-    'April', 'May', 'June', 'July',
-    'August', 'September', 'October',
-    'November', 'December'
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
   ];
 
   var day        = date.getDate();
@@ -181,7 +264,5 @@ function formatDate(date) {
 
   return monthNames[monthIndex] + ' ' + day + ' ' + year;
 }
-
-console.log(formatDate(new Date()));
 
 connection.end();
